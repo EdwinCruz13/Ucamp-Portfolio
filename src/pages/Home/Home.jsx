6d517d99ac42bf04e095d696f0018e3d79df61ad
EdwinC <edwin.cruz@hotmail.com>
2023-04-21 22:41:48 -0600

    adding changes on styles file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,12 +13,13 @@ export const Home = () => {
         <div className="section-body">
           <div className="introducing">
             <h2 className="blast-root">Mis habilidades</h2>
+           
             <p className="blast-description">
               Soy un desarrollador con experiencia en ambientes de desarrollo
               .NET usando lenguajes de programación como C#, vb.Net y ASP.NET
               MVC, además, con el conocimiento en creación y despliegues de
               Restful API en WCF.
-              <br />
+              <br /><br />
               Mis trabajos fueron la creación y despliegues de aplicaciones de
               escritorios en negocios de indelos bancarios, contables y atención
               a público.
@@ -37,22 +38,66 @@ export const Home = () => {
             </p>
           </div>
 
-          <div className="skills">
-            <div className="level-section">
-              <h3>C#</h3>
-            </div>
+          <div className="scored-skill">
+            <section>
+              <h3 className="blast-description">.NET</h3>
+              <div className="level-section level-90">
+                <h3>Asp.Net</h3>
+              </div>
 
-            <div className="level-section">
-              <h3>C#</h3>
-            </div>
+              <div className="level-section level-70">
+                <h3>C#</h3>
+              </div>
 
-            <div className="level-section">
-              <h3>C#</h3>
-            </div>
+              <div className="level-section level-50">
+                <h3>VB.Net</h3>
+              </div>
 
-            <div className="level-section">
-              <h3>C#</h3>
-            </div>
+              <div className="level-section level-30">
+                <h3>Winform - Devexpress</h3>
+              </div>
+
+              <div className="level-section level-50">
+                <h3>WCF</h3>
+              </div>
+
+              <div className="level-section level-10">
+                <h3>SQL-Server</h3>
+              </div>
+            </section>
+
+
+            <section>
+              <h3 className="blast-description">Frontend</h3>
+              <div className="level-section level-90">
+                <h3>ReactJS</h3>
+              </div>
+
+              <div className="level-section level-70">
+                <h3>Html5</h3>
+              </div>
+
+              <div className="level-section level-30">
+                <h3>Css3</h3>
+              </div>
+
+            </section>
+
+            <section>
+              <h3 className="blast-description">Backend</h3>
+              <div className="level-section level-90">
+                <h3>NodeJS</h3>
+              </div>
+
+              <div className="level-section level-70">
+                <h3>ExpressJS</h3>
+              </div>
+
+              
+              <div className="level-section level-10">
+                <h3>Mongo</h3>
+              </div>
+            </section>
           </div>
         </div>
       </section>
